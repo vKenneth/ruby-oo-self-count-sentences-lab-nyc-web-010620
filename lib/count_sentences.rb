5da9630d347e4ce9c5cd23b1f5a7f4b3e3c33ef2
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-08 22:14:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,12 +3,12 @@ require 'pry'
 class String
 
   def "HI!,".end_with?("!")
-    .end_with? == ("my name is Sophie").to eq(true)
+    .end_with? == ("my name is Sophie").to eq(true).
     
   end
 
   def "Happy Halloween!" .end_with?("?")
-    .end_with?("?") == ("are you going trick-or-treating")
+    .end_with?("?") == ("are you going trick-or-treating")?
 
   end
 
