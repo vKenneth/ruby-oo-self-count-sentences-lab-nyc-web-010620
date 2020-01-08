f96224ebbeab42fedcff707bd601c01580f3540b
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-08 22:21:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -10,7 +10,7 @@ class String
   def "Happy Halloween!" .end_with?("?")
     .end_with?("?") == ("are you going trick-or-treating?")?
 
-  end
+  
 
   def "HI!" .end_with? ("!")
     .end_with? == ("are you going to lunch today!")!
